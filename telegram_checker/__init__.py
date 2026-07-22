28ed0b98db5d7cffd32516da6234cfcc152b0fe8
mohamed3lamh <amro42801@gmail.com>
2026-07-23 02:44:42 +0300

    Delete telegram_checker directory

--- telegram_checker/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Telegram Checker Package
-"""
-
-from .telegram_client import TelegramClientManager
-from .account_manager import AccountManager
-from .checker import TelegramChecker
-from .login_manager import LoginManager
-from .flood_manager import FloodManager
