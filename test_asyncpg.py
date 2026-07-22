c276d28bc1987f64d910bd9501fd0797adecb748
mohamed3lamh <amro42801@gmail.com>
2026-07-23 02:42:10 +0300

    Delete test_asyncpg.py

--- test_asyncpg.py
+++
@@ -1,8 +0,0 @@
-import asyncio
-import asyncpg
-import sys
-
-async def test():
-    conn = await asyncpg.connect() # Needs DB url or use a mock. Wait, no db url available directly without reading env.
-    pass
-
